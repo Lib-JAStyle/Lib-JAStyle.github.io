16c7ec6fd9be131b71a43dba74bf0eadbb4e712f
Lib <lib.net.lib+mini@gmail.com>
2020-11-28 20:16:10 +0900

    [Modified] テスト用のダミーデータを追加するデバッグ機能を追加

--- js/debug.js
+++ js/debug.js
@@ -13,3 +13,12 @@ function loadLocalStorage()
 {
   loadDB();
 }
+
+function addDummyGroup()
+{
+  var group = addGroup('リュック', 50);
+  addGroup('小物入れ', 20);
+  addThing(group['id'], 'Macbook Pro', 5);
+  addThing(group['id'], '折り畳み傘', 4);
+  addThing(group['id'], 'クリアファイル', 1);
+}
